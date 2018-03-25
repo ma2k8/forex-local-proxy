bdd531015220390d5f166657368506595991c556
ma2k8 <t_matsukawa@r.recruit.co.jp>
2018-03-26 04:49:25 +0900

    modified Processes.scala for launch of live interpreter

--- src/main/scala/forex/main/Processes.scala
+++ src/main/scala/forex/main/Processes.scala
@@ -1,15 +1,34 @@
 package forex.main
 
-import forex.config._
-import forex.{ services ⇒ s }
-import forex.{ processes ⇒ p }
+import com.softwaremill.sttp.SttpBackend
+import com.softwaremill.sttp.asynchttpclient.monix.AsyncHttpClientMonixBackend
+
+import forex.{ services => s }
+import forex.{ processes => p }
 import org.zalando.grafter.macros._
 
-@readerOf[ApplicationConfig]
-case class Processes() {
+import forex.adapter.http.HttpClient
+import forex.config.{ ApplicationConfig, OneforgeConfig }
+import forex.services.oneforge.api.OneforgeApi
+import monix.cats._
+import monix.eval.Task
 
-  implicit final lazy val _oneForge: s.OneForge[AppEffect] =
-    s.OneForge.dummy[AppStack]
+@readerOf[ApplicationConfig]
+case class Processes(
+    oneforgeConfig: OneforgeConfig
+) {
+
+  implicit final lazy val sttpBackend: SttpBackend[Task, Nothing] = AsyncHttpClientMonixBackend()
+
+  implicit final lazy val oneForge: s.OneForge[AppEffect] =
+    oneforgeConfig.interpreter match {
+      case "dummy" => s.OneForge.dummy[AppStack]
+      case "live"  =>
+        lazy val httpClient = new HttpClient[Task]
+        lazy val api = new OneforgeApi(oneforgeConfig, httpClient)
+        s.OneForge.live[AppStack](api)
+      case _       => throw new RuntimeException(s"no such interpreter[${oneforgeConfig.interpreter}]")
+    }
 
   final val Rates = p.Rates[AppEffect]
 
