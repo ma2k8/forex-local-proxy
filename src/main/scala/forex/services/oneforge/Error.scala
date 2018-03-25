6f1497128c0cc79ab4533044d4c0285fec083b2e
ma2k8 <t_matsukawa@r.recruit.co.jp>
2018-03-26 04:43:05 +0900

    add httpClient

--- src/main/scala/forex/services/oneforge/Error.scala
+++ src/main/scala/forex/services/oneforge/Error.scala
@@ -6,4 +6,6 @@ sealed trait Error extends Throwable with NoStackTrace
 object Error {
   final case object Generic extends Error
   final case class System(underlying: Throwable) extends Error
+  final case class Api(underlying: String) extends Error
+  final case class Json(underlying: io.circe.Error) extends Error
 }
